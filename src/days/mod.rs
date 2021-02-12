28be9e5c83f6b3a2abbee18716f96b29ecfb433b
Karl 'NaN' Wikström <karl@wikstrom.io>
2021-02-12 09:47:56 +0100

    Add day 3

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -8,6 +8,7 @@ use std::boxed::Box;
 
 pub mod day01;
 pub mod day02;
+pub mod day03;
 
 pub fn day_map() -> HashMap<Day,HashMap<Part,Box<dyn Solution>>> {
     vec![
@@ -16,6 +17,9 @@ pub fn day_map() -> HashMap<Day,HashMap<Part,Box<dyn Solution>>> {
                  ].into_iter().collect()),
         (2, vec![(Part1, Box::new(day02::DAY02A) as Box<dyn Solution>),
                  (Part2, Box::new(day02::DAY02B) as Box<dyn Solution>)
+                 ].into_iter().collect()),
+        (3, vec![(Part1, Box::new(day03::DAY03A) as Box<dyn Solution>),
+                 (Part2, Box::new(day03::DAY03B) as Box<dyn Solution>)
                  ].into_iter().collect()),
         ].into_iter().collect()
 }
